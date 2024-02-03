a315b14f44145ee5cfc9273f7cd810200a09ab6f
5jisoo <56earls@gmail.com>
2024-02-03 15:17:54 +0900

    Infra: elasticsearch setting

--- src/main/java/com/wltt/issuetree/global/elasticsearch/config/ElasticsearchConfig.java
+++ src/main/java/com/wltt/issuetree/global/elasticsearch/config/ElasticsearchConfig.java
@@ -1,6 +1,7 @@
 package com.wltt.issuetree.global.elasticsearch.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -10,10 +11,24 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.api-key}")
+    private String elasticsearchAPIKey;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(elasticsearchHost)
+                .usingSsl()
+                .withBasicAuth(username, password)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
