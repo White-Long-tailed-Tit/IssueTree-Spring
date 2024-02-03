632c796c77213dc254f62627bb63eda1425d3d89
5jisoo <56earls@gmail.com>
2024-02-03 10:58:28 +0900

    Initial Setting

---
+++ src/test/java/com/wltt/issuetree/IssueTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wltt.issuetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
