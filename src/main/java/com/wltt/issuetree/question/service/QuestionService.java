08a9d8fe52218d9e34443693bd5e890fb80034be
영서 <dudrhy12@sookmyung.ac.kr>
2024-02-18 02:16:50 +0900

    Feat: SlackAPP 멘션 Service

--- src/main/java/com/wltt/issuetree/question/service/QuestionService.java
+++ src/main/java/com/wltt/issuetree/question/service/QuestionService.java
@@ -1,16 +1,59 @@
 package com.wltt.issuetree.question.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.users.UsersInfoRequest;
+import com.slack.api.methods.response.conversations.ConversationsRepliesResponse;
+import com.slack.api.methods.response.users.UsersInfoResponse;
+import com.slack.api.model.Message;
+import com.wltt.issuetree.question.domain.Question;
 import com.wltt.issuetree.question.domain.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-/*@Service
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
 @RequiredArgsConstructor
 public class QuestionService {
-    //private final QuestionRepository questionRepository;
+    private final String botId = "B06HQ37R80Y";
+    private final QuestionRepository questionRepository;
+    private final MethodsClient methodsClient;
+    private final RestTemplate restTemplate;
 
-*//*
-    public void String appMentionResponse(String text) {
+    @Value("${slack.token}")
+    private String slackToken;
+
+    @Value("${openai.api-key}")
+    private String openaiApiKey;
+
+    public Question appMentionResponse(String ts, String channelId) throws SlackApiException, IOException {
+        Slack slack = Slack.getInstance();
+
+        ConversationsRepliesResponse repliesResponse = slack.methods(slackToken)
+                .conversationsReplies(req -> req
+                        .channel(channelId)
+                        .ts(ts));
+        Question question = CreateQuestion(repliesResponse, ts);
+
+        return question;
     }
+
+
+
+
 }
-*/
+
