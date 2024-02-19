84030f7d4f1a27732a6a3735d82232f13b5d5920
영서 <dudrhy12@sookmyung.ac.kr>
2024-02-20 01:04:55 +0900

    Fix: API 플래그 설정, return 수정

--- src/main/java/com/wltt/issuetree/question/controller/QuestionController.java
+++ src/main/java/com/wltt/issuetree/question/controller/QuestionController.java
@@ -25,26 +25,33 @@ public class QuestionController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final QuestionService questionService;
 
+    private boolean apiInProgress = false;
+
     @PostMapping(value = "/issue", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> eventHandle(@RequestBody Map<String, Object> data) {
+        if (apiInProgress)
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("실행 중인 API가 있습니다.");
         try {
+            apiInProgress = true;
             EventJson eventJson = new EventJson(data);
             eventJson.setJson();
             String ts = eventJson.getTs();
             if (eventJson.getSubType()!=null && eventJson.getSubType().equals("message_changed")) {
                 if (questionService.isTsExist(ts)) {
-                    log.info("이미 처리된 이벤트입니다. 중복 이벤트를 건너뛰고 있습니다.", ts);
-                    return ResponseEntity.ok().body("중복된 이벤트입니다. 이미 처리되었습니다.");
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 이벤트입니다. 이미 처리되었습니다.");
                 }
                 questionService.appMentionResponse(ts, eventJson.getChannel());
+                return new ResponseEntity<>(eventJson.getJson(), HttpStatus.OK);
             }
-
-            return new ResponseEntity<>(eventJson.getJson(), HttpStatus.OK);
+            else
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("편집된 메세지가 아닙니다.");
 
         } catch (SlackApiException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            apiInProgress = false;
         }
     }
 
